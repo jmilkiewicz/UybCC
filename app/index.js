3a9d31c26a4ff0ea5e6e9780b00d7cf27b3cddba
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-06 21:36:28 +0100

    first shitty implementation

---
+++ app/index.js
@@ -0,0 +1,37 @@
+var urlParse = require('url');
+var static = require('node-static');
+
+function isOnlineOfflineQuery(request) {
+	var queryParams = urlParse.parse(request.url, true).query;
+	return request.method === 'GET' && queryParams && !isNaN(queryParams.user_id);
+}
+
+function responseBody(userId) {
+	var serverResponse = {};
+	if (userId % 2 === 0) {
+		serverResponse.online = true;
+	} else {
+		serverResponse.online = false;
+	}
+	return serverResponse;
+}
+
+var fileServer = new static.Server("./www");
+
+module.exports = function (request, response) {
+	fileServer.serve(request, response, function (err, result) {
+		console.log(err);
+		console.log(result);
+	});
+	if(isOnlineOfflineQuery(request)) {
+		setTimeout(function(){
+			response.writeHead(200, {'Content-Type': 'application/json'});
+			var userId = parseInt(urlParse.parse(request.url, true).query.user_id, 10);
+			response.write(JSON.stringify(responseBody(userId)));
+			response.end();
+		}, 500);
+
+
+	}
+
+};
